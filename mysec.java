ab015294d201d5f1466ae73eb551b261e5e504c4
sathish <satish@gmail.com>
2021-09-21 13:45:53 +0530

    hello2

---
+++ mysec.java
@@ -0,0 +1,8 @@
+class Mysec{
+    public static void main(String[]  args) {
+    System.out.println("Hi sir "); 
+    System.out.println("This is my sec app with git")
+     System.out.println("welcome")
+
+    }
+}
